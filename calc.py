167faa4cf7be56c6feda9ee15eb75751ebbaf94a
iamjehaklee <jlee24@babson.edu>
2016-09-01 14:52:00 -0400

    #2

--- calc.py
+++ calc.py
@@ -2,3 +2,20 @@ print(46 + 37 + 38)
 print(2016-1996)
 print(15/3)
 print(2**6)
+#Exercise 2 below
+#1
+minute = 60 #seconds
+print((minute * 42) + 42)
+total_seconds = (minute * 42) + 42
+total_minutes = 42 + (42/60)
+total_hours = (total_minutes/60)
+#2
+mile = 1.61 #km
+print(10/mile)
+miles=(10/mile)
+
+#3
+print('Number 3')
+print(miles/total_seconds)
+print(miles/total_minutes)
+print(miles/total_hours) 
